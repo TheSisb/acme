321b147e47015d7fd136ee0e1615b66555dd5c05
jgoux <hi@jgoux.dev>
2022-05-28 00:50:37 +0200

    CLI

---
+++ apps/cli/dist/index.js
@@ -0,0 +1,7 @@
+// src/index.ts
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+yargs(hideBin(process.argv)).command("curl <url>", "fetch the contents of the URL", () => {
+}, (argv) => {
+  console.info(argv);
+}).demandCommand(1).parse();
--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World");
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+void yargs(hideBin(process.argv))
+  .command(
+    "curl <url>",
+    "fetch the contents of the URL",
+    () => {},
+    (argv) => {
+      console.info(argv);
+    }
+  )
+  .demandCommand(1)
+  .parse();
