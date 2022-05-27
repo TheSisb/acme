1ae7859d571c6ecccd511a39d349b110aac489ef
jgoux <hi@jgoux.dev>
2022-05-28 00:51:22 +0200

    change arg

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -2,13 +2,8 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
 void yargs(hideBin(process.argv))
-  .command(
-    "curl <url>",
-    "fetch the contents of the URL",
-    () => {},
-    (argv) => {
-      console.info(argv);
-    }
-  )
+  .command("curl <url>", "fetch the contents of the URL", undefined, (argv) => {
+    console.info(argv);
+  })
   .demandCommand(1)
   .parse();
