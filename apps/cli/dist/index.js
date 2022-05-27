a960a6f5fc968676392d372463070441fbc809eb
jgoux <hi@jgoux.dev>
2022-05-28 01:12:43 +0200

    oops it shouldn't be commited

--- apps/cli/dist/index.js
+++ apps/cli/dist/index.js
@@ -1,7 +1,6 @@
 // src/index.ts
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
-void yargs(hideBin(process.argv)).command("curl <url>", "fetch the contents of the URL", () => {
-}, (argv) => {
+void yargs(hideBin(process.argv)).command("curl <url>", "fetch the contents of the URL", (argv) => {
   console.info(argv);
 }).demandCommand(1).parse();
