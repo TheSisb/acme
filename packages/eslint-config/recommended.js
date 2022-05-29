ecd71f79b4e695a1df940ea2a81a9d04b6215052
jgoux <hi@jgoux.dev>
2022-05-29 22:28:24 +0200

    tabs vs spaces

--- packages/eslint-config/recommended.js
+++ packages/eslint-config/recommended.js
@@ -45,9 +45,9 @@ module.exports = {
     "unicorn/prefer-module": "error",
     "unicorn/prefer-node-protocol": "error",
     "unused-imports/no-unused-imports": "error",
-		"unused-imports/no-unused-vars": [
-			"warn",
-			{ "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
-		]
+    "unused-imports/no-unused-vars": [
+      "warn",
+      { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
+    ]
   }
 };
