6d4ded173da7216552499ed029c9a98ee6e825db
jgoux <hi@jgoux.dev>
2022-05-30 21:08:32 +0200

    handler isn't needed

--- apps/cli/src/commands/project/index.ts
+++ apps/cli/src/commands/project/index.ts
@@ -4,14 +4,9 @@ import { projectCreateCommand } from "./create.js";
 import { projectListCommand } from "./list.js";
 
 export function projectCommand(yargs: Argv) {
-  return yargs.command(
-    "project",
-    "Projects management",
-    (yargs) => {
-      projectCreateCommand(yargs);
-      projectListCommand(yargs);
-      return yargs;
-    },
-    () => {}
-  );
+  return yargs.command("project", "Projects management", (yargs) => {
+    projectCreateCommand(yargs);
+    projectListCommand(yargs);
+    return yargs;
+  });
 }
