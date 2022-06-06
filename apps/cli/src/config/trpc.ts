d106ce32a1abea17c8dfb5274102a289fb19d93a
jgoux <hi@jgoux.dev>
2022-06-06 20:34:45 +0200

    try to fix dynamic imports for native node modules

--- apps/cli/src/config/trpc.ts
+++ apps/cli/src/config/trpc.ts
@@ -1,6 +1,8 @@
 import type { AppRouter } from "@acme/api";
 import { createTRPCClient } from "@trpc/client";
+import { fetch } from "undici";
 
 export const client = createTRPCClient<AppRouter>({
   url: "http://localhost:4000/trpc",
+  fetch,
 });
