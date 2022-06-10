1378cf0bd1b8c6c82ed78d6eed5c94fa53760c85
jgoux <hi@jgoux.dev>
2022-06-10 17:31:06 +0200

    edit comment

--- packages/scripts/src/commands/build.ts
+++ packages/scripts/src/commands/build.ts
@@ -2,7 +2,7 @@ import { command } from "cleye";
 import { execa } from "execa";
 
 // if the build script fails for this package, replace it with:
-// tsup src/index.ts --format esm,cjs --target node16 --clean --onSuccess 'tsc --project tsconfig.build.json --emitDeclarationOnly --declaration --declarationMap'
+// tsup src/index.ts --tsconfig tsconfig.build.json --format esm,cjs --target node16 --clean --onSuccess 'tsc --project tsconfig.build.json --emitDeclarationOnly --declaration --declarationMap'
 
 export const tsupBuildOptions = [
   "src/index.ts",
